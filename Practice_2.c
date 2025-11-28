9c259dd435f6f76b18cbe8468d1b38a3dcdede94
Nibas2727 <allwebsitegamil@gmail.com>
2025-11-28 11:42:12 +0530

    2nd Question

--- Practice_2.c
+++ Practice_2.c
@@ -3,6 +3,25 @@
 int main(){
 
     int a = 1.999999; // To analyse weather value of a is (1/2) 
-    printf("%d", a);
+    printf("%d \n", a);
 
+    // 5*2 - 2*3
+    // 5*2 / 2*3
+    // 5*(2/2)*3
+    // 5+2/2*3
+
+    int a_1 = 5*2 - 2*3;
+    int a_2 = 5*2 / 2*3;
+    int a_3 = 5*(2/2)*3;
+    int a_4 = 5+2/2*3;
+
+    printf("%d \n", a_1);
+    printf("%d \n", a_2);
+    printf("%d \n", a_3);
+    printf("%d \n", a_4);
+
+    
+
+
+    return 0;
 }
