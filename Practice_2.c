20aea5ee9520dddda86868fc58230d95c3610885
Nibas2727 <allwebsitegamil@gmail.com>
2025-11-29 11:34:51 +0530

    New Questions(3,4,5)

--- Practice_2.c
+++ Practice_2.c
@@ -21,8 +21,23 @@ int main(){
     printf("%d \n", a_3);
     printf("%d \n", a_4);
 
-    
+// ---> (OPERATOR)
+// 3 ---> Write a program to check if a number i divisible by 2 or not
 
+int a_5;
+scanf("\n %d",&a_5);
+printf("\n %d", (a_5%2)==0);
 
-    return 0;
+// 4 ---> write a program to check if a number is odd or even.
+
+scanf("\n %d",&a_5);
+printf("\n %d", (a_5%2)==0);
+
+// 5 ---> Valid / Invalid
+// int a = 8^8; ---> Invalid
+// int x; int y = x; ---> Valid
+// int x, y = x; ---> Valid
+// char starts = '**'; ---> invalid
+
+    return 0; 
 }
